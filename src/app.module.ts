0deb44192e1b9a2eeeec4a450a766e2f6b19e7b1
Tefoh <tefo.ha27@gmail.com>
2024-01-25 22:26:55 +0330

    feat(auth): add auth configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://auth-db-service/nest'),
+    ConfigModule.forRoot(),
     AuthModule,
   ],
   controllers: [AppController],
---
+++ src/auth/config/auth.config.ts
@@ -0,0 +1,10 @@
+import { registerAs } from '@nestjs/config';
+
+const authConfig = registerAs('authConfig', () => ({
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  },
+}));
+
+export default authConfig;
