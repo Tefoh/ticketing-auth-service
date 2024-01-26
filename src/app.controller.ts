e3e5d33b7f5d50b5dc4b355aced56bdb4015e21b
Tefoh <tefo.ha27@gmail.com>
2024-01-26 18:31:33 +0330

    feat(main): add global prefix for all APIs `/api/users`

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,12 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/api/users')
+  @Get()
   getHello() {
     return this.appService.getHello();
-  }
-
-  @UseGuards(AuthGuard('local'))
-  @Post('/api/users/login')
-  async login(@Request() req) {
-    return req.user;
   }
 }
