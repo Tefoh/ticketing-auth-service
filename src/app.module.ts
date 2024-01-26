312a7c4e84df7e53abcb6bee7cc93a0cb1f92d66
Tefoh <tefo.ha27@gmail.com>
2024-01-26 18:27:04 +0330

    feat(hash): import hashing module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { HashingModule } from './hashing/hashing.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://auth-db-service/nest'),
     ConfigModule.forRoot(),
     AuthModule,
+    HashingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
