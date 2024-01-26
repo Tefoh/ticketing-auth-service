4953324a9938d8c0a97aa9da38d03f637a59d1cf
Tefoh <tefo.ha27@gmail.com>
2024-01-26 18:22:28 +0330

    fix(passport): fix passport local strategy by adding the implement abstract class

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -2,9 +2,13 @@ import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from '../auth.service';
+import { PassportInterfaceStrategy } from './passport.interface';
 
 @Injectable()
-export class LocalStrategy extends PassportStrategy(Strategy) {
+export class LocalStrategy
+  extends PassportStrategy(Strategy)
+  implements PassportInterfaceStrategy
+{
   constructor(private authService: AuthService) {
     super({
       usernameField: 'email',
