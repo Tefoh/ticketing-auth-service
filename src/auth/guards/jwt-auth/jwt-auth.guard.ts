561a31a8ec8e3b146f64846d78efe1e96b681c16
Tefoh <tefo.ha27@gmail.com>
2024-01-28 00:54:09 +0330

    feat(jwt-guard): implement the JwtAuthGuard

---
+++ src/auth/guards/jwt-auth/jwt-auth.guard.ts
@@ -0,0 +1,39 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { AuthService } from 'src/auth/auth.service';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly authService: AuthService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    const accessToken = request.cookies?.access_token;
+
+    if (!accessToken) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const token = await this.jwtService.verify(
+        accessToken,
+        this.authService.jwtOptions(),
+      );
+
+      request['user'] = { userId: token.sub, email: token.email };
+
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException();
+    }
+  }
+}
