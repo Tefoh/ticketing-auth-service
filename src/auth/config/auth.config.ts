6a1c63ec93c78c74c7d2b4efff338bd936622bb6
Tefoh <tefo.ha27@gmail.com>
2024-01-28 00:52:53 +0330

    feat(env): add necessary cookie env variable and change the other config variables in auth config

--- src/auth/config/auth.config.ts
+++ src/auth/config/auth.config.ts
@@ -3,10 +3,12 @@ import { registerAs } from '@nestjs/config';
 const authConfig = registerAs('authConfig', () => ({
   jwt: {
     secret: process.env.JWT_SECRET,
-    expiresIn: process.env.JWT_EXPIRES_IN,
+    expiresIn: process.env.JWT_EXPIRES_IN + 'h',
+    expiresInSeconds: +process.env.JWT_EXPIRES_IN * 24 * 60 * 1000,
     issuer: process.env.JWT_ISSUER,
     audience: process.env.JWT_AUDIENCE,
   },
+  secureCookie: !!process.env.AUTH_SECURE_COOKIE,
 }));
 
 export default authConfig;
