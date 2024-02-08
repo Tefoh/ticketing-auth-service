a8456f712cf325cc42ada79fcd0bb54e11c642af
Tefoh <tefo.ha27@gmail.com>
2024-02-09 01:24:06 +0330

    feat(duplicate-exception): implement duplicate exception

--- src/auth/exceptions/duplicate.exception.ts
+++ src/auth/exceptions/duplicate.exception.ts
@@ -1,1 +1,11 @@
-export class DuplicateException extends Error {}
+import { BadRequestException, HttpStatus } from '@nestjs/common';
+
+export class DuplicateException extends BadRequestException {
+  constructor(field: string) {
+    super({
+      message: 'Some fields in form is duplicated!',
+      status: HttpStatus.BAD_REQUEST,
+      description: 'Following fields are duplicated: ' + field,
+    });
+  }
+}
