89f79e5c7ec1ecca0b61f5c103c4056124d9f325
Tefoh <tefo.ha27@gmail.com>
2024-02-10 16:58:17 +0330

    feat(interceptor): add pipe in interceptor to change the response for transformers

--- src/common/response/response.interceptor.ts
+++ src/common/response/response.interceptor.ts
@@ -8,25 +8,37 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Response } from 'express';
 import { JwtCookieParamsType } from '../../auth/types/jwt.interface';
+import { TransformerInterface } from '../interfaces/transformer.interface';
 
 @Injectable()
 export class ResponseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle().pipe(
-      map((value) => {
-        const response = context.switchToHttp().getResponse<Response>();
-        if (value?.['Set-Cookie']) {
-          const setCookieParams: JwtCookieParamsType = value['Set-Cookie'];
-          response.cookie(...setCookieParams);
-          delete value['Set-Cookie'];
-        }
+    return next
+      .handle()
+      .pipe(
+        map((value) => {
+          if (value instanceof TransformerInterface) {
+            return value.toArray();
+          }
 
-        return {
-          message: 'Successful!',
-          status: response.statusCode,
-          data: value?.data ?? value,
-        };
-      }),
-    );
+          return value;
+        }),
+      )
+      .pipe(
+        map((value) => {
+          const response = context.switchToHttp().getResponse<Response>();
+          if (value?.['Set-Cookie']) {
+            const setCookieParams: JwtCookieParamsType = value['Set-Cookie'];
+            response.cookie(...setCookieParams);
+            delete value['Set-Cookie'];
+          }
+
+          return {
+            message: 'Successful!',
+            status: response.statusCode,
+            data: value?.data ?? value,
+          };
+        }),
+      );
   }
 }
