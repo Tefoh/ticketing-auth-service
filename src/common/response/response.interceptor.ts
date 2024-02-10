dfa37e2ef55aabb4a8a43ec5895a28b7f262bec2
Tefoh <tefo.ha27@gmail.com>
2024-02-10 17:28:51 +0330

    refactor(transformers): remove pipe related to transformers

--- src/common/response/response.interceptor.ts
+++ src/common/response/response.interceptor.ts
@@ -13,32 +13,21 @@ import { TransformerInterface } from '../interfaces/transformer.interface';
 @Injectable()
 export class ResponseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next
-      .handle()
-      .pipe(
-        map((value) => {
-          if (value instanceof TransformerInterface) {
-            return value.toArray();
-          }
+    return next.handle().pipe(
+      map((value) => {
+        const response = context.switchToHttp().getResponse<Response>();
+        if (value?.['Set-Cookie']) {
+          const setCookieParams: JwtCookieParamsType = value['Set-Cookie'];
+          response.cookie(...setCookieParams);
+          delete value['Set-Cookie'];
+        }
 
-          return value;
-        }),
-      )
-      .pipe(
-        map((value) => {
-          const response = context.switchToHttp().getResponse<Response>();
-          if (value?.['Set-Cookie']) {
-            const setCookieParams: JwtCookieParamsType = value['Set-Cookie'];
-            response.cookie(...setCookieParams);
-            delete value['Set-Cookie'];
-          }
-
-          return {
-            message: 'Successful!',
-            status: response.statusCode,
-            data: value?.data ?? value,
-          };
-        }),
-      );
+        return {
+          message: 'Successful!',
+          status: response.statusCode,
+          data: value?.data ?? value,
+        };
+      }),
+    );
   }
 }
