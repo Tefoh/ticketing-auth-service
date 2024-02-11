708dabbe9af07d3b9681ba76aef61d4b707a7752
Tefoh <tefo.ha27@gmail.com>
2024-02-11 16:44:15 +0330

    tests(sign-in): implement the sign in unit test functionality

--- src/auth/controllers/sign-in/sign-in.controller.spec.ts
+++ src/auth/controllers/sign-in/sign-in.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SignInController } from './sign-in.controller';
+import { SignInTransformer } from '../../transformers/sign-in.transformer';
+import { CurrentUserTransformer } from '../../transformers/current-user.transformer';
+import { AuthService } from '../../auth.service';
 
 describe('SignInController', () => {
   let controller: SignInController;
@@ -7,6 +10,22 @@ describe('SignInController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SignInController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            signIn: (
+              email: string = 'test@email.com',
+              password: string = 'password',
+            ) => ({
+              email,
+              password,
+            }),
+          },
+        },
+        SignInTransformer,
+        CurrentUserTransformer,
+      ],
     }).compile();
 
     controller = module.get<SignInController>(SignInController);
@@ -14,5 +33,21 @@ describe('SignInController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should login user', async () => {
+    const email = 'test@email.com';
+    const accessToken = 'accessToken';
+    const foundedUser = await controller.signIn({
+      accessToken,
+      user: {
+        _id: '1',
+        email,
+        password: 'password',
+      },
+    });
+
+    expect(foundedUser.user.email).toBe(email);
+    expect(foundedUser.user.id).toBe('1');
   });
 });
