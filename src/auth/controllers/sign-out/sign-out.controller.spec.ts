c9619286386e7326fe6e20bdf439fb1977f38583
Tefoh <tefo.ha27@gmail.com>
2024-02-11 19:39:38 +0330

    feat(sign-out): implement the sign out functionality on controller

--- src/auth/controllers/sign-out/sign-out.controller.spec.ts
+++ src/auth/controllers/sign-out/sign-out.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SignOutController } from './sign-out.controller';
+import { JwtService } from '@nestjs/jwt';
+import { AuthService } from '../../auth.service';
+import { UserService } from '../../user/user.service';
+import { SignOutTransformer } from '../../transformers/sign-out.transformer';
 
 describe('SignOutController', () => {
   let controller: SignOutController;
@@ -7,6 +11,21 @@ describe('SignOutController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SignOutController],
+      providers: [
+        {
+          provide: JwtService,
+          useValue: {},
+        },
+        {
+          provide: AuthService,
+          useValue: {},
+        },
+        {
+          provide: UserService,
+          useValue: {},
+        },
+        SignOutTransformer,
+      ],
     }).compile();
 
     controller = module.get<SignOutController>(SignOutController);
